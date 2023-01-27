0b25e1dc36bb97b836d0e9821f87d2e75811ef68
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-01-28 00:28:15 +0100

    arrow func

---
+++ assigments.js
@@ -0,0 +1,34 @@
+// Lecture 1 - Functions :
+
+// 1. Write a function called 'describeCountry' which takes three parameters:
+// 'country', 'population' and 'capitalCity'. Based on this input, the
+// function returns a string with this format: 'Finland has 6 million people and its
+// capital city is Helsinki'
+// 2. Call this function 3 times, with input data for 3 different countries. Store the
+// returned values in 3 different variables, and log them to the console
+
+// function describeCountry(country, population, capitalCity) {
+//   return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+// }
+
+// const firstCountry = describeCountry('Serbia', 7, 'Belgrade');
+// console.log(firstCountry);
+// const secondCountry = describeCountry('Finland', 6, 'Helsinki');
+// console.log(secondCountry);
+// const thirdCountry = describeCountry('Croatia', 3, 'Zagreb');
+// console.log(thirdCountry);
+
+// LECTURE: Function Declarations vs. Expressions
+
+// 1. The world population is 7900 million people. Create a function declaration
+// called 'percentageOfWorld1' which receives a 'population' value, and
+// returns the percentage of the world population that the given population
+// represents. For example, China has 1441 million people, so it's about 18.2% of
+// the world population
+// 2. To calculate the percentage, divide the given 'population' value by 7900
+// and then multiply by 100
+// 3. Call 'percentageOfWorld1' for 3 populations of countries of your choice,
+// store the results into variables, and log them to the console
+// 4. Create a function expression which does the exact same thing, called
+// 'percentageOfWorld2', and also call it with 3 country populations (can be
+// the same populations)
--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 'use strict';
 
-// Lecture 1 - Functions :
+// Function expression:
 
-// 1. Write a function called 'describeCountry' which takes three parameters:
-// 'country', 'population' and 'capitalCity'. Based on this input, the
-// function returns a string with this format: 'Finland has 6 million people and its
-// capital city is Helsinki'
-// 2. Call this function 3 times, with input data for 3 different countries. Store the
-// returned values in 3 different variables, and log them to the console
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
 
-function describeCountry(country, population, capitalCity) {
-  return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
-}
+// Arrow function:
 
-const firstCountry = describeCountry('Serbia', 7, 'Belgrade');
-console.log(firstCountry);
-const secondCountry = describeCountry('Finland', 6, 'Helsinki');
-console.log(secondCountry);
-const thirdCountry = describeCountry('Croatia', 3, 'Zagreb');
-console.log(thirdCountry);
+const calcAge3 = (birthYear) => 2037 - birthYear;
