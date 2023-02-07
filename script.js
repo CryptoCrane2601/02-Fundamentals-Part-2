8486091d76b76240a64149a79a589ea9479e055b
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-02-07 21:19:29 +0100

    for loop

--- script.js
+++ script.js
@@ -197,3 +197,22 @@
 // 'Dejan is a 48 old salesman, and he has a drivers license.'
 
 // console.log(dejan.getSummary());
+
+// For loop
+
+// console.log('Lifting weights repetition 1 🏋️‍♀️');
+// console.log('Lifting weights repetition 2 🏋️‍♀️');
+// console.log('Lifting weights repetition 3 🏋️‍♀️');
+// console.log('Lifting weights repetition 4 🏋️‍♀️');
+// console.log('Lifting weights repetition 5 🏋️‍♀️');
+// console.log('Lifting weights repetition 6 🏋️‍♀️');
+// console.log('Lifting weights repetition 7 🏋️‍♀️');
+// console.log('Lifting weights repetition 8 🏋️‍♀️');
+// console.log('Lifting weights repetition 9 🏋️‍♀️');
+// console.log('Lifting weights repetition 10 🏋️‍♀️');
+
+// for loop keeps running while condition is TRUE!
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log('Lifting weights repetition 1 🏋️‍♀️');
+}
