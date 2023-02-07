4bff37ebc02480ca68aa653870c588c59232d27e
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-02-07 20:36:15 +0100

    challenge 3 -object methods

--- challenges.js
+++ challenges.js
@@ -69,3 +69,40 @@
 // const total = [tips[0] + bills[0], tips[1] + bills[1], tips[2] + bills[2]];
 
 // console.log(total);
+
+//Coding Challenge #3
+
+// Your tasks:
+// 1. For each of them, create an object with properties for their full name, mass, and
+// height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+// method on both objects). Store the BMI value to a property, and also return it
+// from the method
+// 3. Log to the console who has the higher BMI, together with the full name and the
+// respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+// Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+// tall.
+
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    return this.mass / this.height ** 2;
+  },
+};
+
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    return this.mass / this.height ** 2;
+  },
+};
+
+console.log(
+  `${mark.fullName} BMI (${mark.calcBMI()}) is higher than ${
+    john.fullName
+  } ${john.calcBMI()}`
+);
