90f72f7bc701f30d56d8d9308e4fc34857070a12
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-02-08 20:23:19 +0100

    loop inside loop

--- script.js
+++ script.js
@@ -261,14 +261,21 @@
 
 // Looping backwards and loopping in the loop
 
-const dejan = [
-  'Dejan',
-  'Kurcubic',
-  2023 - 1989,
-  'salesman',
-  ['Nemanja', 'Stojan', 'Igor'],
-];
+// const dejan = [
+//   'Dejan',
+//   'Kurcubic',
+//   2023 - 1989,
+//   'salesman',
+//   ['Nemanja', 'Stojan', 'Igor'],
+// ];
 
-for (let i = dejan.length - 1; i >= 0; i--) {
-  console.log(dejan[i]);
+// for (let i = dejan.length - 1; i >= 0; i--) {
+//   console.log(dejan[i]);
+// }
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------------- Starting exercise ${exercise}; `);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+  }
 }
