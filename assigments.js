9673d75051942230b71814d75dafb3df481231bd
CryptoCrane2601 <dejan.kurcubic@icloud.com>
2023-02-08 20:38:39 +0100

    done assigmnet -looping array

--- assigments.js
+++ assigments.js
@@ -186,6 +186,30 @@
 // Use a for loop to simulate the 50 people voting, by logging a string like this to
 // the console (for numbers 1 to 50): 'Voter number 1 is currently voting'
 
-for (let voter = 1; voter <= 50; voter++) {
-  console.log(`Voter number ${voter} is currently voting`);
+// for (let voter = 1; voter <= 50; voter++) {
+//   console.log(`Voter number ${voter} is currently voting`);
+// }
+
+// LECTURE: Looping Arrays, Breaking and Continuing
+// 1. Let's bring back the 'populations' array from a previous assignment
+// 2. Use a for loop to compute an array called 'percentages2' containing the
+// percentages of the world population for the 4 population values. Use the
+// function 'percentageOfWorld1' that you created earlier
+// 3. Confirm that 'percentages2' contains exactly the same values as the
+// 'percentages' array that we created manually in the previous assignment,
+// and reflect on how much better this solution is
+
+const worldPopulation = 7900;
+const populations = [6, 3, 6.7, 33];
+const percentage2 = [];
+
+function percentageOfWorld1(population) {
+  return (population / worldPopulation) * 100;
 }
+
+for (let i = 0; i < populations.length; i++) {
+  const perc = percentageOfWorld1(populations[i]);
+  percentage2.push(perc);
+}
+
+console.log(percentage2);
